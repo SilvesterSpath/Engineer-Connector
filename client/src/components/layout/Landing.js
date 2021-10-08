af0ba210c8f8ce7393b122095a8e0af41bb42063
SilvesterSpath <spath.sz@gmail.com>
2021-10-08 07:06:56 +0200

    client

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Engineer Connector</h1>
+          <p className='lead'>
+            Create an engineer profile/portfolio, share posts and get help from
+            other engineers
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
