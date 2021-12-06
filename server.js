74434772511afe241c3ebfd473f239a2906afe54
SilvesterSpath <spath.sz@gmail.com>
2021-12-06 08:51:09 +0100

    deploy 4

--- server.js
+++ server.js
@@ -16,10 +16,12 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+const __dirname = path.resolve();
+
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
